ba880a17b6696d221c663fd57be820ecff83a1d7
Ubuntu <ubuntu@ip-172-31-94-194.ec2.internal>
2020-06-23 14:11:10 +0000

    2nd thing

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,37 @@
+package io.pivotal.pal.tracker;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController {
+
+    private Map<String,String> env;
+
+    public EnvController(
+            @Value("${port:NOT SET}") String port,
+            @Value("${memory.limit:NOT SET}") String memoryLimit,
+            @Value("${cf.instance.index:NOT SET}") String cfInstanceIndex,
+            @Value("${cf.instance.addr:NOT SET}") String cfInstanceAddr
+            ){
+
+        env = new HashMap<>();
+        env.put("PORT", port);
+        env.put("MEMORY_LIMIT", memoryLimit);
+        env.put("CF_INSTANCE_INDEX", cfInstanceIndex);
+        env.put("CF_INSTANCE_ADDR", cfInstanceAddr);
+
+    }
+
+    @GetMapping("/env")
+    public Map<String,String> getEnv(){
+        return env;
+    }
+
+
+}
--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,20 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    public WelcomeController( @Value("${welcome.message}") String message ){
+        this.message = message;
+    }
+
+    private String message;
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return message;
     }
 }
