0f4b4d619c9ae55c985f67d6dbe557e8875334ba
Ubuntu <ubuntu@ip-172-31-94-194.ec2.internal>
2020-06-24 20:47:08 +0000

    cloudNativeDeveloperDatabaseAccess

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
 
@@ -12,7 +14,7 @@ public class PalTrackerApplication {
     }
 
     @Bean
-    public TimeEntryRepository getRepository(){
-        return new InMemoryTimeEntryRepository();
+    public TimeEntryRepository getRepository(DataSource dataSource){
+        return new JdbcTimeEntryRepository(dataSource);
     }
 }
